de0958607790b437b3aecd9e5a0a0171dfd2ac82
Harimurti Prasetio <harippe@protonmail.com>
2019-08-20 17:44:49 +0700

    update joda time

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "task-3"
 version := "0.1"
 
 scalaVersion := "2.12.7"
+
+libraryDependencies += "joda-time" % "joda-time" % "2.10.3"
---
+++ src/main/scala/task3/models/CleanStream.scala
@@ -0,0 +1,10 @@
+package task3.models
+
+import org.joda.time.DateTime
+
+case class CleanStream
+(
+  originalId: String,
+  content: String,
+  timestamp: DateTime
+)
